79f3925ebd0aebc30969986a185f54a185fabb0c
katery97 <62404171+katery97@users.noreply.github.com>
2021-01-30 23:56:30 +0800

    Update ShowResults.php

--- app/classes/Transvision/ShowResults.php
+++ app/classes/Transvision/ShowResults.php
@@ -689,7 +689,7 @@ class ShowResults
             }
 
             $filter = "<div id='filters'>
-                <h4>Filter by folder:</h4>
+                <h4>Filter by sub-domain:</h4>
                 <a href='#showall' id='showall' class='filter'>Show all results</a>
                 {$filter}
             </div>\n";
